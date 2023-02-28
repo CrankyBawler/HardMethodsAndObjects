9a165c233904dbbb93a4cd422f5d34b054aa3e4a
Cranky Bawler <6666lenia6666@mail.ru>
2023-02-28 13:58:50 +0300

    1

--- src/Book.java
+++ src/Book.java
@@ -2,4 +2,20 @@ public class Book {
     private String authorName;
     private String bookName;
     private int publishingYear;
+    public Book(String authorName, String bookName, int publishingYear) {
+        this.authorName = authorName;
+        this.bookName = bookName;
+        this.publishingYear = publishingYear;
+    }
+
+    public String getAuthorName() {
+        return this.authorName;
+    }
+
+    public String getBookName() {
+        return this.bookName;
+    }
+    public int getPublishingYear () {
+        return this.publishingYear;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,27 @@
 import java.util.Arrays;
 
 public class Main {
+    public static Book toBook(Book a) {
+        Book[] arr = new Book[5];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                arr[i] = a;
+                System.out.println(arr[i].getAuthorName() + ": " + arr[i].getBookName() + ": " + arr[i].getPublishingYear());
+                break;
+            }
+        }
+        return a;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        // Создать массив типа Book и положить туда созданные книги
-        // Создаем класс Book
-        // создаем массив бук с 5 элементами
-        // Написать методы для работы в этом массиве ( Добавить книгу в свободную ячейку в массиве
-        // и напечатать в консоль в формате “Stephen King: The Stand: 1978”
         Book[] books = new Book[5];
-        System.out.println(Arrays.toString(books));
+        Book one = new Book("Эрих Мария Ремарк", "Возлюби ближнего своего", 1939);
+        Book two = new Book("Грегори Дэвид Робертс", "Шантарам", 2003);
+        Book three = new Book("Джером Дэвид Сэлинджер", "Над пропастью во ржи", 1951);
+        Book four = new Book("Стивен Кинг", "11/22/63", 2011);
+        Book five = new Book("Лев Николаевич Толстой", "Война и мир", 1867);
+        toBook(five);
+        toBook(four);
 
-
-    }
+             }
 }
